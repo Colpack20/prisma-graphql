8f24142fac12c204ef7b9643639c24f210b54028
Daniel Norman <daniel@2color.me>
2020-10-07 16:15:50 +0200

    initial commit

---
+++ src/schema.js
@@ -0,0 +1,67 @@
+const { gql } = require('apollo-server')
+const { prisma } = require('./db')
+
+const typeDefs = gql`
+  type Post {
+    content: String
+    id: ID!
+    published: Boolean!
+    title: String!
+  }
+
+  type Query {
+    feed: [Post!]!
+    post(id: ID!): Post
+  }
+
+  type Mutation {
+    createDraft(content: String, title: String!): Post!
+    publish(id: ID!): Post
+  }
+`
+
+const resolvers = {
+  Query: {
+    feed: (parent, args) => {
+      return prisma.post.findMany({
+        where: { published: true },
+      })
+    },
+    post: (parent, args) => {
+      return prisma.post.findOne({
+        where: { id: Number(args.id) },
+      })
+    },
+  },
+  Mutation: {
+    createDraft: (parent, args) => {
+      return prisma.post.create({
+        data: {
+          title: args.title,
+          content: args.content,
+        },
+      })
+    },
+    publish: (parent, args) => {
+      return prisma.post.update({
+        where: {
+          id: Number(args.id),
+        },
+        data: {
+          published: true,
+        },
+      })
+    },
+  },
+  Post: {
+    content: (parent) => parent.content,
+    id: (parent) => parent.id,
+    published: (parent) => parent.published,
+    title: (parent) => parent.title,
+  },
+}
+
+module.exports = {
+  resolvers,
+  typeDefs,
+}
